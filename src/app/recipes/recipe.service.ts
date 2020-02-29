145191c3979545e90703047c0c9b035982d1571b
Julien Blondeau <julienblondeau@live.fr>
2020-02-29 09:40:15 +0100

    services

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,38 @@
+import { EventEmitter, Injectable } from '@angular/core';
+
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test', 
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('Salad', 6)]
+    ),
+
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test', 
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('Salad', 6)]
+    ),
+  ];
+
+  constructor(private slService: ShoppingListService) {
+
+  }
+
+  getRecipes() {
+    return this.recipes.slice(); // slice to return a new array copied from the original
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+}
